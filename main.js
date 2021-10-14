0022c639e6cf37a404c909fa837e32a04cb98ddb
jeongyongman <i1683u@gmail.com>
2021-10-15 07:51:39 +0900

    JS : navbar menu toggle + project ui

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// Make navbar transprent when it is on the top
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    if(window.scrollY > navbarHeight) {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
@@ -13,18 +13,25 @@ document.addEventListener('scroll', () => {
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click',(event) => {
+navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
-    if(link == null){
+    if (link == null) {
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
 
-// Handel click on "contact me" button on home
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+// Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click',() => {
+homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
@@ -32,7 +39,7 @@ homeContactBtn.addEventListener('click',() => {
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    home.style.opacity = 1- window.scrollY / homeHeight;
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 // Show "arrow up" button when scrolling down
@@ -45,55 +52,43 @@ document.addEventListener('scroll', () => {
     }
 });
 
-// Handel click on the "arrow up" button
-arrowUp.addEventListener('click',() => {
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 });
 
 // Projects
 const workBtnContainer = document.querySelector('.work__categories');
-const ProjectContainer = document.querySelector('.work__projects');
-const projects =  document.querySelectorAll('.project');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
 workBtnContainer.addEventListener('click', (e) => {
     const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-    if(filter == null){
+    if (filter == null) {
         return;
     }
     
     // Remove selection from the previous item and select the new one
     const active = document.querySelector('.category__btn.selected');
-    active.classList.remove('selected');
-    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-    target.classList.add('selected');
+    if (active != null) {
+        active.classList.remove('selected');
+      }
+      e.target.classList.add('selected');
 
-    ProjectContainer.classList.add('anim-out');
-    
+    projectContainer.classList.add('anim-out');
     setTimeout(() => {
         projects.forEach((project) => {
             console.log(project.dataset.type);
-            if(filter === '*' || filter === project.dataset.type){
+            if (filter === '*' || filter === project.dataset.type) {
                 project.classList.remove('invisible');
             } else {
                 project.classList.add('invisible');
             }
         });
-        ProjectContainer.classList.remove('anim-out');
-    },300);
-
-    // console.log('-------------------');
-    // for(let project of projects) {
-    //     console.log(project);
-    // }
-
-    // console.log('-------------------');
-    // let project;
-    // for (let i = 1; i < projects.length; i++){
-    //     project = projects[i];
-    //     console.log(project);
-    // }
+        projectContainer.classList.remove('anim-out');
+    }, 300);
 });
 
-function scrollIntoView(selector){
+function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior:"smooth"});
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
